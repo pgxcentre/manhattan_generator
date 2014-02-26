3dcadd9436a7fca248879dd207988c6037f44bc3
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2014-02-26 11:46:59 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# manhattan_generator - Manhattan Plot Generator #
+
+_manhattan_generator_ is a python tool to create beautiful Manhattan plots.
+
+
+
+## Dependencies ##
+
+Here are the dependencies that must be installed before _py_megablast_:
+
+*   [Python][1] (>= 2.7, but not 3.0)
+*   [numpy][2] (>= 1.8.0)
+*   [matplotlib[3] (>= 1.3.1)
+
+
+
+## Installation ##
+
+For Linux users, we recommend installing _manhattan_generator_ in a Python
+[virtualenv][4] (virtual environment).
+
+_manhattan_generator_ should work on Windows and MacOS, even though it hasn't
+been fully tested for full compatibility.
+
+
+[1]: http://python.org/
+[2]: http://www.numpy.org/
+[3]: http://matplotlib.org/
+[4]: http://pypi.python.org/pypi/virtualenv
---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+# How to build source distribution
+# python setup.py sdist --format bztar
+# python setup.py sdist --format gztar
+# python setup.py sdist --format zip
+
+import os
+from setuptools import setup
+
+# Check the Python version
+import sys
+major, minor, micro, s, tmp = sys.version_info
+if major==2 and minor<7 or major<2:
+    raise SystemExit("""Requires Python 2.7 or later.""")
+if major==3:
+    raise SystemExit("""Doesn't work on Python 3...""")
+
+setup(name="manhattan_generator",
+      version="1.0",
+      description="Creation of beautiful Manhattan plots",
+      author="Louis-Philippe Lemieux Perreault",
+      author_email="louis-philippe.lemieux.perreault@statgen.org",
+      url="http://www.statgen.org",
+      license="GPL",
+      scripts=[os.path.join("scripts", "manhattan_generator"),],
+      install_requires=["matplotlib >=1.3.1", "numpy >= 1.8.0"],
+      classifiers=['Operating System :: Linux',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2.7'])
