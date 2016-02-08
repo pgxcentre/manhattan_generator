3b16d1649219515833be448b1ea5be8a523267ea
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2016-02-08 09:35:38 -0500

    Module refactoring

--- setup.py
+++ setup.py
@@ -5,26 +5,38 @@
 # python setup.py sdist --format gztar
 # python setup.py sdist --format zip
 
-import os
+
 from setuptools import setup
 
-# Check the Python version
-import sys
-major, minor, micro, s, tmp = sys.version_info
-if major==2 and minor<7 or major<2:
-    raise SystemExit("""Requires Python 2.7 or later.""")
-if major==3:
-    raise SystemExit("""Doesn't work on Python 3...""")
 
-setup(name="manhattan_generator",
-      version="1.6",
-      description="Creation of beautiful Manhattan plots",
-      author="Louis-Philippe Lemieux Perreault",
-      author_email="louis-philippe.lemieux.perreault@statgen.org",
-      url="http://www.statgen.org",
-      license="GPL",
-      scripts=[os.path.join("scripts", "manhattan_generator"),],
-      install_requires=["matplotlib >=1.3.1", "numpy >= 1.8.0"],
-      classifiers=['Operating System :: Linux',
-                   'Programming Language :: Python',
-                   'Programming Language :: Python :: 2.7'])
+VERSION="1.6.0"
+
+
+def setup_package():
+    setup(
+        name="manhattan_generator",
+        version=VERSION,
+        description="Creation of beautiful Manhattan plots",
+        author="Louis-Philippe Lemieux Perreault",
+        author_email="louis-philippe.lemieux.perreault@statgen.org",
+        url="https://github.com/pgxcentre/manhattan_generator",
+        license="CC BY-NC 4.0",
+        entry_points={
+            "console_scripts": [
+                "manhattan_generator=manhattan_generator:main",
+            ],
+        },
+        py_modules=["cli"],
+        install_requires=["matplotlib >=1.3.1", "numpy >= 1.8.0"],
+        classifiers=[
+            "Operating System :: Linux",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2.7",
+        ],
+    )
+
+    return
+
+
+if __name__ == "__main__":
+    setup_package()
