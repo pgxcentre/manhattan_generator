2b70ca09148284c621de4f6518fec4e46a5e741e
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2016-03-16 11:31:50 -0400

    Fixes #1: now reads data by chunk (using pandas read_csv iterator).

--- manhattan_generator.py
+++ manhattan_generator.py
@@ -222,7 +222,10 @@ def read_input_file(i_fn, use_bp, use_p, options):
 
     """
     # Reading the data
-    data = pd.read_csv(i_fn, sep="\t", low_memory=False)
+    csv_iterator = pd.read_csv(i_fn, sep="\t", chunksize=1e6)
+    data = next(csv_iterator).dropna()
+    for chunk in csv_iterator:
+        data = data.append(chunk.dropna(), ignore_index=True)
 
     # Checking we have the required column
     required_cols = {options.col_chr, options.col_name,
